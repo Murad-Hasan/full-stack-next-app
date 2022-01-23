3e1e2f01f7a7d9edea0578f0dec4908c70e9c3da
Al Mahamud Murad <muradhasan.7845@gmail.com>
2022-01-23 21:38:56 +0600

    login system return null

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
@@ -8,12 +8,11 @@ const prisma = new PrismaClient();
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
-    })
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
   ],
-  
   callbacks: {
     async signIn({}) {
       return "/logged";
--- pages/logged.tsx
+++ pages/logged.tsx
@@ -1,9 +1,8 @@
-import { getSession } from "next-auth/react";
+import { getSession } from "next-auth/react"
 
 const loggedPage = ({ session }) => {
   console.log(session);
-  // const { name, email } = session.user;
-  return <div>Welcome </div>;
+  return <div>Welcome</div>;
 };
 
 export default loggedPage;
@@ -11,14 +10,15 @@ export default loggedPage;
 export async function getServerSideProps({ req, res }) {
   //is currently logged in
   const session = await getSession({ req });
-  // if (!session) {
-  //   return {
-  //     redirect: {
-  //       destination: "/",
-  //       permanent: false,
-  //     },
-  //   };
-  // }
+  console.log(session);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
   return {
     props: {
